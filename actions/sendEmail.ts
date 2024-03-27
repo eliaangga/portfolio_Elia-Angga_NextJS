794d922acdab5cf05600d768471feb2053b81ef8
eliaangga <100867891+eliaangga@users.noreply.github.com>
2024-03-27 16:28:18 +0700

    minor fix

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,13 +5,14 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  // validasi server
+  // validatable email addresss 
   if (!validateString(senderEmail, 500)) {
     return {
       error: "Invalid sender email",
--- components/intro.tsx
+++ components/intro.tsx
@@ -104,6 +104,8 @@ export default function Intro() {
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/eliaangga/"
           target="_blank"
+          rel="noopener"
+          title="LinkedIn Profile"
         >
           <BsLinkedin />
         </a>
@@ -112,6 +114,8 @@ export default function Intro() {
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/eliaangga"
           target="_blank"
+          rel="noopener"
+          title="GitHub Profile"
         >
           <FaGithubSquare />
         </a>
